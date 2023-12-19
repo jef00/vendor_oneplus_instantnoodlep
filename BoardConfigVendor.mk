9c471d1f26d3ea1557984c9ab5169368c1ae6f49
jeffrey <jeffrey_jagram_90@hotmail.com>
2023-12-19 16:00:01 +0100

    Rework firmware including for PE

--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-FIRMWARE_IMAGES := $(wildcard vendor/oneplus/firmware/$(PRODUCT_DEVICE)/*)
+FIRMWARE_IMAGES := $(wildcard vendor/oneplus/$(PRODUCT_DEVICE)/radio/*)
 
 AB_OTA_PARTITIONS += \
     $(foreach f, $(notdir $(FIRMWARE_IMAGES)), $(basename $(f)))
